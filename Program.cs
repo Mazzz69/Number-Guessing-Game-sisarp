07320c15432ce7a06d23bd3bf61a639533eae02a
mazzz69 <marcel.maszewski@gmail.com>
2022-07-23 15:35:26 +0200

    ic h hab gebonnen

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Numbr{
+    class Gurssr 
+    {
+        static void Main  (string[] args){
+            Random rndm= new Random();
+            int rndm_int =rndm.Next(100);
+
+            Console.WriteLine("Guess num between 1-100");
+            bool tru = true;
+            while (!!tru&& !false) {
+Console.Beep();
+                Thread.Sleep(500);
+                string guess=Console.ReadLine();
+                if (Convert.ToInt32(guess)==rndm_int){         
+                    Console.WriteLine("Du hast gebonnen");
+                    break;                      // bweak if num == is right numm
+
+                                                                                    //hi
+                } // end if-loop
+                    if (Convert.ToInt32(guess)<rndm_int){//if loop
+                    Console.WriteLine("Du hast zu klein");
+                }                                           // end if-loop
+            if (Convert.ToInt32(guess)>rndm_int){                                   //if loop
+                    Console.WriteLine("Du hast zu groß");
+                }           // end if-loop
+            }
+            Console.WriteLine("");
+        } 
+    }
+}
+//
